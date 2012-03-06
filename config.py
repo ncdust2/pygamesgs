72779729b582f5308f3c01308708dc601a30c258
ncdust2 <87658356@qq.com>
2012-03-06 19:17:13 +0800

    add config

---
+++ config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+TITLETEXT = '三国杀'
+
+SCREENWIDTH = 1280
+SCREENHEIGHTH = 800
+MAXFPS = 50
+
+BUTTONWIDTH = 100
+BUTTONHEIGHTH = 50
+BUTTONIMGURL = 'img/button.jpg'
+BUTTONNAME = 'unnamed'
+
+SUITIMG = 'img/suit'
+SUITX = 20
+SUITY = 20
+BIGSUITIMG = 'img/bigsuit'
+BIGSUITX = 30
+BIGSUITY = 30
+
+CARDLIST = [('slash',3,'club')]
+CARDIMG = 'img/card'
+BIGCARDIMG = 'img/bigcard'
+
+BACKGROUD = 'img/pubu.jpg'
+MOUSE = 'img/chain.png'
